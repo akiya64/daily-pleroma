0fe134728dedd688a1d6f2a30de8aafaa7416ddf
Akiya <autumnsky64@gmail.com>
2025-04-27 10:51:16 +0900

    Add plugin info

---
+++ daily-pleroma.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Plugin Name: daily-pleroma
+ * Description: from pleroma / akkoma instance via RSS feed.
+ * Version:     0.0.1
+ * Author: Akiya
+ * Author URI:  https://autumunsky.jp/
+ * TextDomain:  daily-pleroma
+ *
+ * @package:     daily-pleroma
+ */
+
+
