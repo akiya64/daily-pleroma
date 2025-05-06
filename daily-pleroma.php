e3d232c1004e600df138f383efd18e25beb71a3d
Akiya <autumnsky64@gmail.com>
2025-05-06 20:45:37 +0900

    Fix plugin header comment

--- daily-pleroma.php
+++ daily-pleroma.php
@@ -1,10 +1,10 @@
 <?php
 /**
- * Plugin Name: daily-pleroma
- * Description: from pleroma / akkoma instance via RSS feed.
+ * Plugin Name: Daily pleroma
+ * Description: Post daily digest from pleroma / akkoma instance via RSS feed.
  * Version:     0.0.1
  * Author: Akiya
- * Author URI:  https://autumunsky.jp/
+ * Author URI:  https://code.autumunsky.jp/akiya/
  * TextDomain:  daily-pleroma
  *
  * @package:     daily-pleroma
