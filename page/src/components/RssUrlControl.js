13ef15c3367ac49a7c52cbee98c7f04aee1b7d9c
Akiya <autumnsky64@gmail.com>
2026-06-09 14:34:27 +0900

    Fix coding style

--- page/src/components/RssUrlControl.js
+++ page/src/components/RssUrlControl.js
@@ -1,14 +1,12 @@
-import { __ } from '@wordpress/i18n';
-import { TextControl } from '@wordpress/components';
+import { __ } from '@wordpress/i18n'
+import { TextControl } from '@wordpress/components'
 
 export const RssUrlControl = ( { value, onChange }) => {
 	return (
 		<TextControl
 			label={ __( 'rss の URL', 'daily-preloma' ) }
 			value={ value }
-
 			onChange={ onChange }
-
 		/>
 	)
 }
--- page/src/index.js
+++ page/src/index.js
@@ -1,5 +1,5 @@
-import domReady from '@wordpress/dom-ready';
-import { createRoot } from '@wordpress/element';
+import domReady from '@wordpress/dom-ready'
+import { createRoot } from '@wordpress/element'
 import { RssUrlControl } from './components/RssUrlControl'
 import { AuthorSelector } from './components/AuthorSelector'
 import { CategorySelector } from './components/CategorySelector'
@@ -18,7 +18,7 @@ const SettingsPage = () => {
 domReady( () => {
     const root = createRoot(
         document.getElementById( 'daily-pleroma-settings' )
-    );
+    )
 
-    root.render( <SettingsPage /> );
-} );
+    root.render( <SettingsPage /> )
+} )
