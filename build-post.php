9531577f2a92a71776b5d58034ab296bedca4ee2
Akiya <autumnsky64@gmail.com>
2026-06-12 17:07:07 +0900

    Refactor for prepare test end point

--- build-post.php
+++ build-post.php
@@ -5,45 +5,34 @@
  * @package daily-pleroma
  */
 
-function build_main_content( DateTime $date, $all_items = array() ){
-	ksort($all_items);
-	$items = array_filter( $all_items, function( $k ) use ( $date ){
-		return str_contains( $k, $date->format( 'Y-m-d' ) );
-	}, ARRAY_FILTER_USE_KEY );
+function build_daily_digest_post( DateTime $date, $all_items = array() ) {
+	if( ! $all_items ){
+		$all_items = parse_pleroma_atom( get_option( 'rss_url' ) );
+	}
 
-	ksort( $items );
+	$items = slice_items( $date, $all_items );
 
-	$content = '';
+	if( ! $items ){
+		return;
+	}
+
+	$main_content = '';
 	foreach( $items as $item ){
-		$content .= <<< EOF
+		$main_content .= <<< EOF
 			<!-- wp:paragraph -->
 			<p>{$item['content']} <a href="{$item['link']}" target="_blank">#</a></p>
 			<!-- /wp:paragraph -->
 			EOF;
 	}
 
-	return $content;
-}
-
-function build_daily_digest_post( DateTime $date, $all_items = array() ) {
-	if( ! $all_items ){
-		$all_items = parse_pleroma_atom( get_option( 'rss_url' ) );
-	}
-
 	$date_string = $date->format( 'Y-m-d' );
 
-	$main_content = build_main_content( $date, $all_items );
-
-	if( $main_content ){
-		return array(
-			'post_name' => 'from_akkoma_' . $date_string,
-			'post_title' => 'From akkoma ' . $date_string,
-			'post_content' => $main_content,
-			'post_status' => 'publish',
-			'post_author' => get_option( 'digest_author' ) ?? '',
-			'post_category' => array( get_option( 'digest_category' ) ?? '' ),
-		);
-	} else {
-		return;
-	}
+	return array(
+		'post_name' => 'from_akkoma_' . $date_string,
+		'post_title' => 'From akkoma ' . $date_string,
+		'post_content' => $main_content,
+		'post_status' => 'publish',
+		'post_author' => get_option( 'digest_author' ) ?? '',
+		'post_category' => array( get_option( 'digest_category' ) ?? '' ),
+	);
 }
--- helper.php
+++ helper.php
@@ -17,3 +17,15 @@ function exists_digest_post( DateTime $date ){
 
 	return $posts ? true : false;
 }
+
+function slice_items( DateTime $date, $all_items = array() ){
+	ksort($all_items);
+
+	$items = array_filter( $all_items, function( $k ) use ( $date ){
+		return str_contains( $k, $date->format( 'Y-m-d' ) );
+	}, ARRAY_FILTER_USE_KEY );
+
+	ksort( $items );
+
+	return $items;
+}
