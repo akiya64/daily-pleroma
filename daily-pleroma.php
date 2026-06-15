b3446e044f07fad110adc99ee26098bb30c5e973
Akiya <autumnsky64@gmail.com>
2026-06-15 19:05:14 +0900

    Remove unused php

--- daily-pleroma.php
+++ daily-pleroma.php
@@ -10,7 +10,6 @@
  * @package:     daily-pleroma
  */
 
-require_once __DIR__ . '/action/test-settings.php';
 require_once __DIR__ . '/action/post-from-json.php';
 require_once __DIR__ . '/page/settings.php';
 
