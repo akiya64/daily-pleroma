e1ce0275302d017a54785115b673aee623732a22
Akiya <autumnsky64@gmail.com>
2026-06-16 16:04:59 +0900

    Make dir rest api

--- daily-pleroma.php
+++ daily-pleroma.php
@@ -11,11 +11,11 @@
  */
 
 require_once __DIR__ . '/action/post-from-json.php';
+require_once __DIR__ . '/rest-api/fetch-rss.php';
 require_once __DIR__ . '/page/settings.php';
 
 require_once __DIR__ . '/build-post.php';
 require_once __DIR__ . '/helper.php';
 require_once __DIR__ . '/parser.php';
 require_once __DIR__ . '/register-settings.php';
-require_once __DIR__ . '/rest-api.php';
 require_once __DIR__ . '/scheduler.php';
