ce8e3a86170b5cb18ffb334ab536cfae0826d686
Akiya <autumnsky64@gmail.com>
2026-07-03 14:34:37 +0900

    Create functions dir

--- daily-pleroma.php
+++ daily-pleroma.php
@@ -10,14 +10,14 @@
  * @package:     daily-pleroma
  */
 
+require_once __DIR__ . '/functions/build-post.php';
+require_once __DIR__ . '/functions/helper.php';
+require_once __DIR__ . '/functions/parser.php';
+require_once __DIR__ . '/functions/register-settings.php';
+require_once __DIR__ . '/functions/scheduler.php';
+
 require_once __DIR__ . '/rest-api/fetch-rss.php';
 require_once __DIR__ . '/rest-api/import-json.php';
 
 require_once __DIR__ . '/page/settings/index.php';
 require_once __DIR__ . '/page/tools/index.php';
-
-require_once __DIR__ . '/build-post.php';
-require_once __DIR__ . '/helper.php';
-require_once __DIR__ . '/parser.php';
-require_once __DIR__ . '/register-settings.php';
-require_once __DIR__ . '/scheduler.php';
